5c15e08c6f6223cf3ddd9e5a83d6b0f4c9b3b260
michelly-wilges <michellyvg@yahoo.com.br>
2025-11-09 16:13:42 -0300

    Remove commented-out code in Principal.java
    Comentários removidos

--- src/main/java/br/edu/infnet/michellyapi/principal/Principal.java
+++ src/main/java/br/edu/infnet/michellyapi/principal/Principal.java
@@ -117,21 +117,4 @@ public class Principal {
                 System.out.println("Opção inválida! Tente novamente.");
         }
     }
-
-
-
-//        if (opcao.toLowerCase().contains("cad")) {
-//            System.out.println("Digite 1 para Cliente e 2 para Funcionário: ");
-//        } else if (opcao.toLowerCase().contains("age")) {
-//            System.out.println("Digite o nome do funcionário: ");
-//        } else {
-//            System.out.println("Digite o nome do cliente para consultar: ");
-//        }
-//
-//        Cliente cliente1 = new Cliente("Odete Roitman", "123.456.789-00", "(21)99234-5678");
-//        Cliente cliente2 = new Cliente("Maria de Fátima Acioly", "789.456.123-00", "(21)91234-5678");
-//        Cliente cliente3 = new Cliente("Celina juqueira","456.123.789-00", "(51)99234-5678");
-//
-//        System.out.println(cliente1);
-
 }
